469a5b01b45931f9d2ca4daae895414f6603b117
hietane <61339413+hietane@users.noreply.github.com>
2020-02-22 23:14:16 +0200

    Add files via upload

---
+++ TFV_PATCHES/config.cpp
@@ -0,0 +1,47 @@
+class CfgPatches
+{
+	class TFV_Patches
+	{
+		author="P1ker1";
+		hideName=0;
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+		};
+	};
+};
+class CfgUnitInsignia
+{
+	class TFV_spartan_01_og
+	{
+		displayName = "Spartan 0-1 (Brown & White)"; 
+		author = "P1ker1"; 
+		texture = "\TFV_Patches\data\TFV_spartan01_og_ca.paa"; 
+	};
+	class TFV_spartan_01_sd
+	{
+		displayName = "Spartan 0-1 (Subdued)"; 
+		author = "P1ker1"; 
+		texture = "\TFV_Patches\data\TFV_spartan01_sd_2_ca.paa"; 
+	};
+	class TFV_spartan_01_br
+	{
+		displayName = "Spartan 0-1 (Black & Red)"; 
+		author = "P1ker1"; 
+		texture = "\TFV_Patches\data\TFV_spartan01_br_ca.paa"; 
+	};
+	class TFV_reaper01_bw_ca
+	{
+		displayName = "Reaper 0-1 (Black & White, Placeholder)"; 
+		author = "P1ker1"; 
+		texture = "\TFV_Patches\data\TFV_reaper01_bw_ca.paa"; 
+	};
+	class TFV_160thSOAR_ATLAS_1
+	{
+		displayName = "160th SOAR, Atlas (Colored)"; 
+		author = "P1ker1"; 
+		texture = "\TFV_Patches\data\TFV_160thSOAR_ATLAS_1.paa"; 
+	};
+};
